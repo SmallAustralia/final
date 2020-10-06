4f3daf7741658258853c11262f183972cc155a68
SmallAustralia <48656368+SmallAustralia@users.noreply.github.com>
2020-10-06 20:11:53 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,8 +2,8 @@
 
 
 function myFunction(imgs) {
-  var expandImg = document.getElementById("expandedImg");
-  var imgText = document.getElementById("imgtext");
+  let expandImg = document.getElementById("expandedImg");
+  let imgText = document.getElementById("imgtext");
   expandImg.src = imgs.src;
   imgText.innerHTML = imgs.alt;
   expandImg.parentElement.style.display = "block";
